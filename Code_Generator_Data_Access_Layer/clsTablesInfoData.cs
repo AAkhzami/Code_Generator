e707e28a5002faa839b93b92fc93fa029ca949be
abdullrahman <lodragon71@gmail.com>
2026-07-28 14:05:48 +0400

    Add classes for database and table info retrieval
    Added `clsTablesInfoData` and `clsUserDatabasesData` classes to the `Code_Generator_Data_Access_Layer` namespace. These classes provide methods to retrieve table names from a specified database and user-created databases on the SQL Server instance, respectively.
    
    Updated `Code_Generator_Data_Access_Layer.csproj` to include the new classes. Modified `.cache` files and updated binary files (`.dll` and `.pdb`) for `Code_Generator_Business_Layer` and `Code_Generator_Data_Access_Layer` to reflect the changes.
    
    Note: Exception handling in the new methods is currently minimal, as the `catch` blocks are empty.

---
+++ Code_Generator_Data_Access_Layer/clsTablesInfoData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Code_Generator_Data_Access_Layer
+{
+    public class clsTablesInfoData
+    {
+        static public DataTable GetAllTablesNameByDatabaseName(string DatabaseName)
+        {
+            DataTable dt = new DataTable();
+            string query = @"
+                        SELECT TABLE_NAME as Name
+                        FROM INFORMATION_SCHEMA.TABLES 
+                        WHERE TABLE_TYPE = 'BASE TABLE' ";
+            using (SqlConnection connection = new SqlConnection(clsDataAccessConnections.ConnectionsString.Replace("master", DatabaseName)))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            dt.Load(reader);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return dt;
+        }
+    }
+}
---
+++ Code_Generator_Data_Access_Layer/clsUserDatabasesData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Generator_Data_Access_Layer
+{
+    public class clsUserDatabasesData
+    {
+        public static DataTable GetAllDatabasesOnDevice()
+        {
+            DataTable dt = new DataTable();
+
+            string query = "SELECT Name FROM sys.databases WHERE database_id > 4 ORDER BY name";
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessConnections.ConnectionsString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            dt.Load(reader);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            return dt;
+        }
+    }
+}
