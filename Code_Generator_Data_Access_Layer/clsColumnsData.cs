88a1904ed6fce8ad26cef2028f633beb4a5a6993
abdullrahman <lodragon71@gmail.com>
2026-07-28 14:21:06 +0400

    Add clsColumnsData class for retrieving table column names
    A new file, `clsColumnsData.cs`, has been added, introducing the `clsColumnsData` class. This class provides a static method, `GetAllColumnsNameByTableName`, to fetch column names for a specified table in a database using a SQL query. The method uses parameterized queries to enhance security and returns the results as a `DataTable`.
    
    The project file `Code_Generator_Data_Access_Layer.csproj` has been updated to include the new class. Binary files (`Code_Generator_Data_Access_Layer.dll` and `Code_Generator_Data_Access_Layer.pdb`) and the compilation cache file have been updated to reflect the changes.

---
+++ Code_Generator_Data_Access_Layer/clsColumnsData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Code_Generator_Data_Access_Layer
+{
+    public class clsColumnsData
+    {
+        static public DataTable GetAllColumnsNameByTableName(string DatabaseName,string TableName)
+        {
+            DataTable dt = new DataTable();
+            string query = $@"
+                        Select 
+                        c.name as [ColumnName]
+                        From sys.columns c
+                        inner join sys.types t on c.user_type_id = t.user_type_id
+                        where c.object_id = OBJECT_ID(@TableName)";
+            using (SqlConnection connection = new SqlConnection(clsDataAccessConnections.ConnectionsString.Replace("master", DatabaseName)))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@TableName", TableName);
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            dt.Load(reader);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return dt;
+        }
+    }
+}
