d68d9d9659e68604dfcf8bfefda6905bf35bc2ce
abdullrahman <lodragon71@gmail.com>
2026-07-31 14:36:35 +0400

    Add clsBusinessLayerGenerator for dynamic property generation
    A new class, `clsBusinessLayerGenerator`, has been added to the project to facilitate the generation of business layer code based on database and table structures. This class includes functionality to dynamically generate property definitions for table columns and supports operation modes (`AddNew` and `Update`).
    
    The project file (`Code_Generator_Business_Layer.csproj`) has been updated to include the new class. Binary files (`Code_Generator_Business_Layer.dll` and `Code_Generator_Business_Layer.pdb`) and the compilation cache have been updated to reflect these changes.

---
+++ Code_Generator_Business_Layer/clsBusinessLayerGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Code_Generator_Business_Layer.clsGlobal;
+
+namespace Code_Generator_Business_Layer
+{
+    public class clsBusinessLayerGenerator
+    {
+        string _database;
+        string _table;
+        clsColumnModelBuilder _Columns;
+        public clsBusinessLayerGenerator(string Database, string Table)
+        {
+            _database = Database;
+            _table = Table;
+            _Columns = new clsColumnModelBuilder(_database, _table);
+        }
+
+        public string WriteProperties()
+        {
+            StringBuilder propertiesText = new StringBuilder();
+            propertiesText.AppendLine("public enum enMode { AddNew = 0, Update = 1 };");
+            propertiesText.AppendLine("public enMode Mode = enMode.AddNew;");
+            foreach (clsColumnModelBuilder.strColumnInfo column in _Columns.GetAllColumnsInfo())
+            {
+                propertiesText.AppendLine($"public {column.ColumnType} {column.ColumnName} {{get;set;}}");
+            }
+            return propertiesText.ToString();
+        }
+    }
+}
