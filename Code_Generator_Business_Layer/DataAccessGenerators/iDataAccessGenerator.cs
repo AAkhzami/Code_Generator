fdb060d0e745208e8ed734212ec4d8924aff6301
abdullrahman <lodragon71@gmail.com>
2026-08-01 21:23:31 +0400

    Add GenerateReadAllRecordsMethods to interface
    Added a `using System.Data;` directive to include the `System.Data` namespace for working with `DataTable`. Introduced the `GenerateReadAllRecordsMethods()` method in the `iDataAccessGenerator` interface to retrieve all records as a `DataTable`.

--- Code_Generator_Business_Layer/DataAccessGenerators/iDataAccessGenerator.cs
+++ Code_Generator_Business_Layer/DataAccessGenerators/iDataAccessGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace Code_Generator_Business_Layer.DataAccessGenerators
         string GenerateReadMethod();
         string GenerateUpdateMethod();
         string GenerateDeleteMethod();
+        DataTable GenerateReadAllRecordsMethods();
 
     }
 }
