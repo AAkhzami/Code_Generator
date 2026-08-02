82ccf28f86a0ac21b2ed120977d6f3253467d440
abdullrahman <lodragon71@gmail.com>
2026-08-02 18:06:29 +0400

    Add ClassCodeBuilder for dynamic DAL generation
    A new `ClassCodeBuilder` class has been introduced to generate data access layer (DAL) classes dynamically based on database, table, connection type, and operation type.
    
    The class includes an `enOperationType` enum for defining operations (`Insert`, `Update`, `Delete`, `Select`, `All`) and a `GenerateDataAccessLayerClass` method that builds the appropriate methods using `clsSQLServerDataAccessLayerGenerator`.
    
    The project file (`Code_Generator_Business_Layer.csproj`) has been updated to include `ClassCodeBuilder.cs`. Binary files (`Code_Generator_Business_Layer.dll` and `.pdb`) and the compilation cache have been updated to reflect these changes.

---
+++ Code_Generator_Business_Layer/ClassCodeBuilder.cs
@@ -0,0 +1,45 @@
+﻿using Code_Generator_Business_Layer.DataAccessGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Generator_Business_Layer
+{
+    internal class ClassCodeBuilder
+    {
+        public enum enOperationType
+        {
+            Insert = 0,
+            Update = 1,
+            Delete = 2,
+            Select = 3,
+            All = 4,
+        } 
+        public static string GenerateDataAccessLayerClass(string Database, string Table, clsConnectionGenerator connectionType, enOperationType operationType)
+        {
+            StringBuilder sb = new StringBuilder();
+            clsSQLServerDataAccessLayerGenerator dataAccessLayer = new clsSQLServerDataAccessLayerGenerator(Database, Table, connectionType);
+            switch(operationType)
+            {
+                case enOperationType.Insert:
+                    sb.Append(dataAccessLayer.GenerateCreateMethod());
+                    break;
+                case enOperationType.Update:
+                    sb.Append(dataAccessLayer.GenerateUpdateMethod());
+                    break;
+                case enOperationType.Delete:
+                    sb.Append(dataAccessLayer.GenerateDeleteMethod());
+                    break;
+                case enOperationType.Select:
+                    sb.Append(dataAccessLayer.GenerateReadMethod());
+                    break;
+                case enOperationType.All:
+                    sb.Append(dataAccessLayer.GenerateDataAccessLayer());
+                    break;
+            }
+            return sb.ToString();
+        }
+    }
+}
