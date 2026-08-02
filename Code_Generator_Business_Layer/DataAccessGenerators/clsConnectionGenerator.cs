9c32b0e428a512af5de414c2475394cf9e4322ca
abdullrahman <lodragon71@gmail.com>
2026-08-02 17:47:41 +0400

    Add clsConnectionGenerator for connection handling logic
    A new file, `clsConnectionGenerator.cs`, was added to the project and included in the `.csproj` file. This class, located in the `Code_Generator_Business_Layer.DataAccessGenerators` namespace, introduces functionality for generating database connections.

---
+++ Code_Generator_Business_Layer/DataAccessGenerators/clsConnectionGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Generator_Business_Layer.DataAccessGenerators
+{
+    internal class clsConnectionGenerator
+    {
+        public enum enConnectionType
+        {
+            StaticClass = 0,
+            AppConfig = 1,
+        }
+
+        enConnectionType _connectionType = enConnectionType.StaticClass;
+        string _connectionString = string.Empty;
+        string _location = string.Empty;
+        string _databaseName = string.Empty;
+        string _userName = string.Empty;
+        string _password = string.Empty;
+
+        public clsConnectionGenerator(enConnectionType connectionType, string Location, string DatabaseName, string UserName, string Password)
+        {
+            _connectionType = connectionType;
+            _location = Location;
+            _databaseName = DatabaseName;
+            _userName = UserName;
+            _password = Password;
+        }
+    }
+}
