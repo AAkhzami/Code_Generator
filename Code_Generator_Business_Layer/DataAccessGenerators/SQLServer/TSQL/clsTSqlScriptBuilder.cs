24ae19ce8d8764b6bd987da1f70bd3a1482d2585
abdullrahman <lodragon71@gmail.com>
2026-08-06 14:27:10 +0400

    Refactor clsTSqlScriptBuilder and update project structure
    The `clsTSqlScriptBuilder.cs` file was rewritten to introduce a functional class for generating SQL scripts. The new implementation uses `clsColumnModelBuilder` to dynamically retrieve column metadata and includes methods for generating scripts to fetch all records or records by primary key.
    
    The file was moved to `DataAccessGenerators\SQLServer\TSQL` in the project structure. Updates were made to `Code_Generator_Business_Layer.csproj` to reflect this change.
    
    Binary files and cache files were updated due to the code changes and recompilation.

---
+++ Code_Generator_Business_Layer/DataAccessGenerators/SQLServer/TSQL/clsTSqlScriptBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Generator_Business_Layer
+{
+    public class clsTSqlScriptBuilder
+    {
+        string _TableName;
+        string _Database;
+        clsColumnModelBuilder _ColumnsInfo;
+        List<clsColumnModelBuilder.strColumnInfo> _ColumnsList;
+
+        public clsTSqlScriptBuilder(string Database,string Table)
+        {
+            _TableName = Table;
+            _Database = Database;
+
+            _ColumnsInfo = new clsColumnModelBuilder(_Database, _TableName);
+            _ColumnsList = _ColumnsInfo.GetAllColumnsInfo();
+        }
+        public string CreateGetAllRecordsScript()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Create Procedure GetAll{_TableName}");
+            sb.AppendLine("as");
+            sb.AppendLine("begin");
+            sb.AppendLine("\tSET NOCOUNT ON;");
+            sb.AppendLine($"\tselect * from {_TableName};");
+            sb.AppendLine("end");
+
+            return sb.ToString();
+        }
+        public string CreateGetRecordByPrimaryKey()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Create Procedure GetBy{_ColumnsInfo.PrimaryKey.ColumnName}");
+
+            sb.AppendLine(clsHelper.FormatingProperties(
+                _ColumnsList.Where(c => c.IsPrimaryKey == true).Select(c => $"@{c.ColumnName} {c.ColumnSqlType}").ToList()));
+            sb.AppendLine("as");
+            sb.AppendLine("begin");
+            sb.AppendLine("\tSET NOCOUNT ON;");
+            sb.AppendLine($"select * from [{_TableName}]");
+            sb.AppendLine("where");
+            sb.AppendLine(string.Join(" and ",_ColumnsList.Where(c => c.IsPrimaryKey == true).Select(c => $"[{c.ColumnName}] = @{c.ColumnName}").ToList()));
+            sb.AppendLine("end");
+
+            return sb.ToString();
+        }
+    }
+}
