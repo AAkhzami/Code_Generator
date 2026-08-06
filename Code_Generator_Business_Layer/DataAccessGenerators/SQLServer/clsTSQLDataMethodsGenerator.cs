1450ef14111359f659da77a603557b1056c74f81
abdullrahman <lodragon71@gmail.com>
2026-08-06 20:27:31 +0400

    Add clsTSQLDataMethodsGenerator class for SQL generation
    A new file, `clsTSQLDataMethodsGenerator.cs`, was added to the project. This file introduces the `clsTSQLDataMethodsGenerator` class, which implements the `iDataAccessGenerator` interface.
    
    The class provides placeholder methods for generating SQL data access methods, including `GenerateCreateMethod`, `GenerateUpdateMethod`, `GenerateDeleteMethod`, `GenerateReadMethod`, `GenerateReadAllRecordsMethod`, and `GenerateDataAccessLayerClass`. All methods currently return empty strings.
    
    The project file `Code_Generator_Business_Layer.csproj` was updated to include the new file.

---
+++ Code_Generator_Business_Layer/DataAccessGenerators/SQLServer/clsTSQLDataMethodsGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Generator_Business_Layer.DataAccessGenerators.SQLServer
+{
+    public class clsTSQLDataMethodsGenerator : iDataAccessGenerator
+    {
+        public string GenerateCreateMethod()
+        {
+            return "";
+        }
+        public string GenerateUpdateMethod()
+        {
+            return "";
+        }
+        public string GenerateDeleteMethod()
+        {
+            return "";
+        }
+        public string GenerateReadMethod()
+        {
+            return "";
+        }
+        public string GenerateReadAllRecordsMethod()
+        {
+            return "";
+        }
+        public string GenerateDataAccessLayerClass()
+        {
+            return "";
+        }
+    }
+}
