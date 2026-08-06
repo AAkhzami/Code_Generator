7f113c078454c8ccfcaf29a3b1e5fdd9f9e1cddf
abdullrahman <lodragon71@gmail.com>
2026-08-06 20:15:37 +0400

    Add clsTSqlScriptExecutor for executing T-SQL scripts
    A new class `clsTSqlScriptExecutor` was added to the project in the `Code_Generator_Business_Layer.DataAccessGenerators.SQLServer.TSQL` namespace. This class provides methods to execute single or multiple T-SQL scripts against a SQL Server database.
    
    The `ExecuteScript` method allows executing a single script, while the `ExecuteScripts` method handles a list of scripts. Both methods use `SqlConnection` and `SqlCommand` to interact with the database, with exception handling to ensure robustness.
    
    The project file `Code_Generator_Business_Layer.csproj` was updated to include the new class file. Binary files (`Code_Generator_Business_Layer.dll` and `.pdb`) and the `CoreCompileInputs.cache` file were updated to reflect the changes.

---
+++ Code_Generator_Business_Layer/DataAccessGenerators/SQLServer/TSQL/clsTSqlScriptExecutor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Generator_Business_Layer.DataAccessGenerators.SQLServer.TSQL
+{
+    public class clsTSqlScriptExecutor
+    {
+        public static bool ExecuteScript(string script, string connectionString)
+        {
+            return ExecuteScripts(new List<string> { script}, connectionString);                 
+        }
+
+        public static bool ExecuteScripts(List<string> scripts, string connectionString)
+        {            
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    foreach (string script in scripts)
+                    {
+                        using (SqlCommand command = new SqlCommand(script, connection))
+                        {
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+            
+        }
+    }
+}
