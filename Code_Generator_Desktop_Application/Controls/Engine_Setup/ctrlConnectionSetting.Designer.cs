682f62978b730eefa2eb8a201ca2a43d1266d559
Just_Abdullrahman <abdullrahman.ak23@gmail.com>
2026-08-17 00:03:17 +0400

    Improve ctrlConnectionSetting UI layout and spacing
    Updated control to use larger sizes, margins, and padding for improved spacing and appearance. Increased panel and control sizes, adjusted font sizes, and enhanced overall scaling for a more visually appealing interface.

--- Code_Generator_Desktop_Application/Controls/Engine_Setup/ctrlConnectionSetting.Designer.cs
+++ Code_Generator_Desktop_Application/Controls/Engine_Setup/ctrlConnectionSetting.Designer.cs
@@ -70,9 +70,10 @@
             this.guna2Panel1.Controls.Add(this.label5);
             this.guna2Panel1.Controls.Add(this.label4);
             this.guna2Panel1.Controls.Add(this.flowLayoutPanel1);
-            this.guna2Panel1.Location = new System.Drawing.Point(3, 3);
+            this.guna2Panel1.Location = new System.Drawing.Point(4, 4);
+            this.guna2Panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(492, 460);
+            this.guna2Panel1.Size = new System.Drawing.Size(656, 566);
             this.guna2Panel1.TabIndex = 1;
             // 
             // guna2Panel5
@@ -80,17 +81,19 @@
             this.guna2Panel5.BorderRadius = 9;
             this.guna2Panel5.Controls.Add(this.pictureBox3);
             this.guna2Panel5.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(219)))), ((int)(((byte)(229)))));
-            this.guna2Panel5.Location = new System.Drawing.Point(35, 47);
+            this.guna2Panel5.Location = new System.Drawing.Point(19, 28);
+            this.guna2Panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Panel5.Name = "guna2Panel5";
-            this.guna2Panel5.Size = new System.Drawing.Size(50, 50);
+            this.guna2Panel5.Size = new System.Drawing.Size(67, 62);
             this.guna2Panel5.TabIndex = 22;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::Code_Generator_DApp.Properties.Resources.url;
-            this.pictureBox3.Location = new System.Drawing.Point(7, 7);
+            this.pictureBox3.Location = new System.Drawing.Point(9, 9);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(36, 36);
+            this.pictureBox3.Size = new System.Drawing.Size(48, 44);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 14;
             this.pictureBox3.TabStop = false;
@@ -100,9 +103,10 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(139)))), ((int)(((byte)(153)))));
-            this.label5.Location = new System.Drawing.Point(93, 77);
+            this.label5.Location = new System.Drawing.Point(124, 70);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(370, 38);
+            this.label5.Size = new System.Drawing.Size(464, 48);
             this.label5.TabIndex = 21;
             this.label5.Text = "Choose how the connection string will be managed\r\nin your application.";
             // 
@@ -111,9 +115,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Tahoma", 21F);
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(34)))), ((int)(((byte)(48)))));
-            this.label4.Location = new System.Drawing.Point(91, 43);
+            this.label4.Location = new System.Drawing.Point(121, 28);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(392, 34);
+            this.label4.Size = new System.Drawing.Size(492, 42);
             this.label4.TabIndex = 20;
             this.label4.Text = "Connection String Architecture";
             // 
@@ -123,10 +128,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.flowLayoutPanel1.Controls.Add(this.pAppConfig);
             this.flowLayoutPanel1.Controls.Add(this.pStaticClass);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 154);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(4, 190);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(486, 244);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(648, 300);
             this.flowLayoutPanel1.TabIndex = 19;
             // 
             // pAppConfig
@@ -142,9 +148,10 @@
             this.pAppConfig.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(252)))), ((int)(((byte)(249)))));
             this.pAppConfig.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(252)))), ((int)(((byte)(249)))));
             this.flowLayoutPanel1.SetFlowBreak(this.pAppConfig, true);
-            this.pAppConfig.Location = new System.Drawing.Point(3, 3);
+            this.pAppConfig.Location = new System.Drawing.Point(4, 4);
+            this.pAppConfig.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pAppConfig.Name = "pAppConfig";
-            this.pAppConfig.Size = new System.Drawing.Size(483, 115);
+            this.pAppConfig.Size = new System.Drawing.Size(644, 142);
             this.pAppConfig.TabIndex = 8;
             this.pAppConfig.Click += new System.EventHandler(this.pDirectQueries_Click);
             // 
@@ -154,9 +161,10 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(139)))), ((int)(((byte)(153)))));
-            this.label3.Location = new System.Drawing.Point(89, 67);
+            this.label3.Location = new System.Drawing.Point(119, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(306, 19);
+            this.label3.Size = new System.Drawing.Size(378, 24);
             this.label3.TabIndex = 21;
             this.label3.Text = "Store connection string in App.Config file.";
             // 
@@ -166,9 +174,10 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Tahoma", 18F);
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(34)))), ((int)(((byte)(48)))));
-            this.label6.Location = new System.Drawing.Point(88, 29);
+            this.label6.Location = new System.Drawing.Point(117, 36);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(126, 29);
+            this.label6.Size = new System.Drawing.Size(161, 36);
             this.label6.TabIndex = 20;
             this.label6.Text = "App.Config";
             // 
@@ -180,9 +189,10 @@
             this.rbAppConfig.CheckedState.BorderThickness = 0;
             this.rbAppConfig.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(147)))), ((int)(((byte)(91)))));
             this.rbAppConfig.CheckedState.InnerColor = System.Drawing.Color.White;
-            this.rbAppConfig.Location = new System.Drawing.Point(435, 28);
+            this.rbAppConfig.Location = new System.Drawing.Point(580, 34);
+            this.rbAppConfig.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbAppConfig.Name = "rbAppConfig";
-            this.rbAppConfig.Size = new System.Drawing.Size(34, 24);
+            this.rbAppConfig.Size = new System.Drawing.Size(45, 30);
             this.rbAppConfig.TabIndex = 19;
             this.rbAppConfig.Text = "guna2CustomRadioButton4";
             this.rbAppConfig.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
@@ -196,18 +206,20 @@
             this.guna2Panel4.BorderRadius = 9;
             this.guna2Panel4.Controls.Add(this.pictureBox2);
             this.guna2Panel4.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(148)))), ((int)(((byte)(91)))));
-            this.guna2Panel4.Location = new System.Drawing.Point(20, 25);
+            this.guna2Panel4.Location = new System.Drawing.Point(27, 31);
+            this.guna2Panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Panel4.Name = "guna2Panel4";
-            this.guna2Panel4.Size = new System.Drawing.Size(65, 65);
+            this.guna2Panel4.Size = new System.Drawing.Size(87, 80);
             this.guna2Panel4.TabIndex = 13;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = global::Code_Generator_DApp.Properties.Resources.code;
-            this.pictureBox2.Location = new System.Drawing.Point(8, 8);
+            this.pictureBox2.Location = new System.Drawing.Point(11, 10);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox2.Size = new System.Drawing.Size(64, 59);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
@@ -219,9 +231,10 @@
             this.guna2CustomRadioButton2.CheckedState.BorderThickness = 0;
             this.guna2CustomRadioButton2.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.guna2CustomRadioButton2.CheckedState.InnerColor = System.Drawing.Color.White;
-            this.guna2CustomRadioButton2.Location = new System.Drawing.Point(556, 11);
+            this.guna2CustomRadioButton2.Location = new System.Drawing.Point(741, 14);
+            this.guna2CustomRadioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CustomRadioButton2.Name = "guna2CustomRadioButton2";
-            this.guna2CustomRadioButton2.Size = new System.Drawing.Size(34, 24);
+            this.guna2CustomRadioButton2.Size = new System.Drawing.Size(45, 30);
             this.guna2CustomRadioButton2.TabIndex = 0;
             this.guna2CustomRadioButton2.Text = "guna2CustomRadioButton2";
             this.guna2CustomRadioButton2.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
@@ -242,9 +255,10 @@
             this.pStaticClass.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(254)))));
             this.pStaticClass.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(254)))));
             this.flowLayoutPanel1.SetFlowBreak(this.pStaticClass, true);
-            this.pStaticClass.Location = new System.Drawing.Point(3, 124);
+            this.pStaticClass.Location = new System.Drawing.Point(4, 154);
+            this.pStaticClass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pStaticClass.Name = "pStaticClass";
-            this.pStaticClass.Size = new System.Drawing.Size(483, 115);
+            this.pStaticClass.Size = new System.Drawing.Size(644, 142);
             this.pStaticClass.TabIndex = 9;
             this.pStaticClass.Click += new System.EventHandler(this.pStaticClass_Click);
             // 
@@ -254,9 +268,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(139)))), ((int)(((byte)(153)))));
-            this.label1.Location = new System.Drawing.Point(90, 67);
+            this.label1.Location = new System.Drawing.Point(120, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(304, 19);
+            this.label1.Size = new System.Drawing.Size(386, 24);
             this.label1.TabIndex = 21;
             this.label1.Text = "Manage connection string in a static class.";
             // 
@@ -266,9 +281,10 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Tahoma", 18F);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(34)))), ((int)(((byte)(48)))));
-            this.label2.Location = new System.Drawing.Point(89, 29);
+            this.label2.Location = new System.Drawing.Point(119, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(133, 29);
+            this.label2.Size = new System.Drawing.Size(165, 36);
             this.label2.TabIndex = 20;
             this.label2.Text = "Static Class";
             // 
@@ -279,9 +295,10 @@
             this.rbStaticClass.CheckedState.BorderThickness = 0;
             this.rbStaticClass.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(64)))), ((int)(((byte)(199)))));
             this.rbStaticClass.CheckedState.InnerColor = System.Drawing.Color.White;
-            this.rbStaticClass.Location = new System.Drawing.Point(435, 28);
+            this.rbStaticClass.Location = new System.Drawing.Point(580, 34);
+            this.rbStaticClass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbStaticClass.Name = "rbStaticClass";
-            this.rbStaticClass.Size = new System.Drawing.Size(34, 24);
+            this.rbStaticClass.Size = new System.Drawing.Size(45, 30);
             this.rbStaticClass.TabIndex = 19;
             this.rbStaticClass.Text = "guna2CustomRadioButton4";
             this.rbStaticClass.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
@@ -295,18 +312,20 @@
             this.guna2Panel2.BorderRadius = 9;
             this.guna2Panel2.Controls.Add(this.pictureBox1);
             this.guna2Panel2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(62)))), ((int)(((byte)(196)))));
-            this.guna2Panel2.Location = new System.Drawing.Point(20, 25);
+            this.guna2Panel2.Location = new System.Drawing.Point(27, 31);
+            this.guna2Panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Panel2.Name = "guna2Panel2";
-            this.guna2Panel2.Size = new System.Drawing.Size(65, 65);
+            this.guna2Panel2.Size = new System.Drawing.Size(87, 80);
             this.guna2Panel2.TabIndex = 13;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::Code_Generator_DApp.Properties.Resources.idea;
-            this.pictureBox1.Location = new System.Drawing.Point(8, 8);
+            this.pictureBox1.Location = new System.Drawing.Point(11, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.Size = new System.Drawing.Size(64, 59);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -318,9 +337,10 @@
             this.guna2CustomRadioButton3.CheckedState.BorderThickness = 0;
             this.guna2CustomRadioButton3.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.guna2CustomRadioButton3.CheckedState.InnerColor = System.Drawing.Color.White;
-            this.guna2CustomRadioButton3.Location = new System.Drawing.Point(556, 11);
+            this.guna2CustomRadioButton3.Location = new System.Drawing.Point(741, 14);
+            this.guna2CustomRadioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CustomRadioButton3.Name = "guna2CustomRadioButton3";
-            this.guna2CustomRadioButton3.Size = new System.Drawing.Size(34, 24);
+            this.guna2CustomRadioButton3.Size = new System.Drawing.Size(45, 30);
             this.guna2CustomRadioButton3.TabIndex = 0;
             this.guna2CustomRadioButton3.Text = "guna2CustomRadioButton3";
             this.guna2CustomRadioButton3.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
@@ -330,11 +350,12 @@
             // 
             // ctrlConnectionSetting
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.guna2Panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ctrlConnectionSetting";
-            this.Size = new System.Drawing.Size(498, 469);
+            this.Size = new System.Drawing.Size(664, 577);
             this.guna2Panel1.ResumeLayout(false);
             this.guna2Panel1.PerformLayout();
             this.guna2Panel5.ResumeLayout(false);
