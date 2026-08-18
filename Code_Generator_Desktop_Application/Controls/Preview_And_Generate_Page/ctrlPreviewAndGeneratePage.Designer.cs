98c19419cabd208d45fc3b851b4c3929f486a175
abdullrahman <lodragon71@gmail.com>
2026-08-18 15:28:29 +0400

    Add FastColoredTextBox package and create Preview_And_Generate_Page controls
    - Added FastColoredTextBox library dependency
    - Created Controls/Preview_And_Generate_Page directory structure
    - Added ctrlPreviewAndGeneratePage user control

---
+++ Code_Generator_Desktop_Application/Controls/Preview_And_Generate_Page/ctrlPreviewAndGeneratePage.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Code_Generator_DApp.Controls.Preview_And_Generate_Page
+{
+    partial class ctrlPreviewAndGeneratePage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ctrlPreviewAndGeneratePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ctrlPreviewAndGeneratePage";
+            this.Size = new System.Drawing.Size(1389, 655);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Code_Generator_Desktop_Application/Controls/Preview_And_Generate_Page/ctrlPreviewAndGeneratePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Code_Generator_DApp.Controls.Preview_And_Generate_Page
+{
+    public partial class ctrlPreviewAndGeneratePage : UserControl
+    {
+        public ctrlPreviewAndGeneratePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
