fb1b4a263c13d6087909dfb24709f8411dd16625
abdullrahman <lodragon71@gmail.com>
2026-08-18 16:04:26 +0400

    Add Guna2Panel and TabControl to Preview Page UI
    Introduced a new `Guna2Panel` and `Guna2TabControl` to the `ctrlPreviewAndGeneratePage` control, providing a tabbed interface with four tabs: `DataAccessClass`, `BusinessLayerClass`, `Queries`, and `ConnectionType`.
    
    Updated the control layout to include the new panel and adjusted its size. Added private fields and public properties to dynamically update tab names. Fixed minor issues in property setters to ensure proper functionality.
    
    Updated the `DesignTimeResolveAssemblyReferences.cache` binary file due to changes in project references during the design-time build process.

--- Code_Generator_Desktop_Application/Controls/Preview_And_Generate_Page/ctrlPreviewAndGeneratePage.Designer.cs
+++ Code_Generator_Desktop_Application/Controls/Preview_And_Generate_Page/ctrlPreviewAndGeneratePage.Designer.cs
@@ -28,18 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.tbCodesGenerator = new Guna.UI2.WinForms.Guna2TabControl();
+            this.tpDataAccessClass = new System.Windows.Forms.TabPage();
+            this.tpBusinessLayer = new System.Windows.Forms.TabPage();
+            this.tpQueries = new System.Windows.Forms.TabPage();
+            this.tpConnection = new System.Windows.Forms.TabPage();
+            this.guna2Panel1.SuspendLayout();
+            this.tbCodesGenerator.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2Panel1.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Panel1.BorderRadius = 8;
+            this.guna2Panel1.BorderThickness = 1;
+            this.guna2Panel1.Controls.Add(this.tbCodesGenerator);
+            this.guna2Panel1.Location = new System.Drawing.Point(3, 3);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(1371, 574);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // tbCodesGenerator
+            // 
+            this.tbCodesGenerator.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCodesGenerator.Controls.Add(this.tpDataAccessClass);
+            this.tbCodesGenerator.Controls.Add(this.tpBusinessLayer);
+            this.tbCodesGenerator.Controls.Add(this.tpQueries);
+            this.tbCodesGenerator.Controls.Add(this.tpConnection);
+            this.tbCodesGenerator.ItemSize = new System.Drawing.Size(180, 40);
+            this.tbCodesGenerator.Location = new System.Drawing.Point(3, 3);
+            this.tbCodesGenerator.Name = "tbCodesGenerator";
+            this.tbCodesGenerator.SelectedIndex = 0;
+            this.tbCodesGenerator.Size = new System.Drawing.Size(1365, 568);
+            this.tbCodesGenerator.TabButtonHoverState.BorderColor = System.Drawing.Color.Empty;
+            this.tbCodesGenerator.TabButtonHoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.tbCodesGenerator.TabButtonHoverState.Font = new System.Drawing.Font("Segoe UI Semibold", 10F);
+            this.tbCodesGenerator.TabButtonHoverState.ForeColor = System.Drawing.Color.White;
+            this.tbCodesGenerator.TabButtonHoverState.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.tbCodesGenerator.TabButtonIdleState.BorderColor = System.Drawing.Color.Empty;
+            this.tbCodesGenerator.TabButtonIdleState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(42)))), ((int)(((byte)(57)))));
+            this.tbCodesGenerator.TabButtonIdleState.Font = new System.Drawing.Font("Segoe UI Semibold", 10F);
+            this.tbCodesGenerator.TabButtonIdleState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(160)))), ((int)(((byte)(167)))));
+            this.tbCodesGenerator.TabButtonIdleState.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(42)))), ((int)(((byte)(57)))));
+            this.tbCodesGenerator.TabButtonSelectedState.BorderColor = System.Drawing.Color.Empty;
+            this.tbCodesGenerator.TabButtonSelectedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(37)))), ((int)(((byte)(49)))));
+            this.tbCodesGenerator.TabButtonSelectedState.Font = new System.Drawing.Font("Segoe UI Semibold", 10F);
+            this.tbCodesGenerator.TabButtonSelectedState.ForeColor = System.Drawing.Color.White;
+            this.tbCodesGenerator.TabButtonSelectedState.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(132)))), ((int)(((byte)(255)))));
+            this.tbCodesGenerator.TabButtonSize = new System.Drawing.Size(180, 40);
+            this.tbCodesGenerator.TabIndex = 0;
+            this.tbCodesGenerator.TabMenuBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(42)))), ((int)(((byte)(57)))));
+            this.tbCodesGenerator.TabMenuOrientation = Guna.UI2.WinForms.TabMenuOrientation.HorizontalTop;
+            // 
+            // tpDataAccessClass
+            // 
+            this.tpDataAccessClass.Location = new System.Drawing.Point(4, 44);
+            this.tpDataAccessClass.Name = "tpDataAccessClass";
+            this.tpDataAccessClass.Size = new System.Drawing.Size(1357, 520);
+            this.tpDataAccessClass.TabIndex = 0;
+            this.tpDataAccessClass.Text = "DataAccessClass";
+            this.tpDataAccessClass.UseVisualStyleBackColor = true;
+            // 
+            // tpBusinessLayer
+            // 
+            this.tpBusinessLayer.Location = new System.Drawing.Point(4, 44);
+            this.tpBusinessLayer.Name = "tpBusinessLayer";
+            this.tpBusinessLayer.Size = new System.Drawing.Size(1357, 520);
+            this.tpBusinessLayer.TabIndex = 1;
+            this.tpBusinessLayer.Text = "BusinessLayerClass";
+            this.tpBusinessLayer.UseVisualStyleBackColor = true;
+            // 
+            // tpQueries
+            // 
+            this.tpQueries.Location = new System.Drawing.Point(4, 44);
+            this.tpQueries.Name = "tpQueries";
+            this.tpQueries.Size = new System.Drawing.Size(1357, 520);
+            this.tpQueries.TabIndex = 2;
+            this.tpQueries.Text = "Queries";
+            this.tpQueries.UseVisualStyleBackColor = true;
+            // 
+            // tpConnection
+            // 
+            this.tpConnection.Location = new System.Drawing.Point(4, 44);
+            this.tpConnection.Name = "tpConnection";
+            this.tpConnection.Size = new System.Drawing.Size(1357, 520);
+            this.tpConnection.TabIndex = 3;
+            this.tpConnection.Text = "ConnectionType";
+            this.tpConnection.UseVisualStyleBackColor = true;
             // 
             // ctrlPreviewAndGeneratePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.guna2Panel1);
             this.Name = "ctrlPreviewAndGeneratePage";
-            this.Size = new System.Drawing.Size(1389, 655);
+            this.Size = new System.Drawing.Size(1377, 580);
+            this.guna2Panel1.ResumeLayout(false);
+            this.tbCodesGenerator.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2TabControl tbCodesGenerator;
+        private System.Windows.Forms.TabPage tpDataAccessClass;
+        private System.Windows.Forms.TabPage tpBusinessLayer;
+        private System.Windows.Forms.TabPage tpQueries;
+        private System.Windows.Forms.TabPage tpConnection;
     }
 }
--- Code_Generator_Desktop_Application/Controls/Preview_And_Generate_Page/ctrlPreviewAndGeneratePage.cs
+++ Code_Generator_Desktop_Application/Controls/Preview_And_Generate_Page/ctrlPreviewAndGeneratePage.cs
@@ -12,6 +12,60 @@ namespace Code_Generator_DApp.Controls.Preview_And_Generate_Page
 {
     public partial class ctrlPreviewAndGeneratePage : UserControl
     {
+
+        private string _DataAccessClass = "DataAccessClass";
+        private string _BusinessLayerClass = "BusinessLayerClass";
+        private string _Queries = "Queries";
+        private string _Connection = "ConnectionType";
+        public string DataAccessClass
+        {
+            get
+            {
+                return _DataAccessClass;
+            }
+            set
+            {
+                _DataAccessClass = value;
+                tbCodesGenerator.TabPages["tpDataAccessClass"].Text = _DataAccessClass;
+            }
+        }
+        public string BusinessLayerClass
+        {
+            get
+            {
+                return _BusinessLayerClass;
+            }
+            set
+            {
+                _BusinessLayerClass = value;
+                tbCodesGenerator.TabPages["tbBusinessLayerClass"].Text = _BusinessLayerClass;
+            }
+        }
+        public string Querys
+        {
+            get
+            {
+                return _Queries;
+            }
+            set
+            {
+                _Queries = value;                
+                tbCodesGenerator.TabPages["tpQueries"].Text = _BusinessLayerClass;
+            }
+        }
+        public string Connection
+        {
+            get
+            {
+                return _Connection;
+            }
+            set
+            {
+                _Connection = value;                
+                tbCodesGenerator.TabPages["tpConnection"].Text = _BusinessLayerClass;
+            }
+        }
+
         public ctrlPreviewAndGeneratePage()
         {
             InitializeComponent();
