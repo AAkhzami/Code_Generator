df3fbe68b2689f5efc739670fb8246dc5ac08057
abdullrahman <lodragon71@gmail.com>
2026-08-19 01:15:13 +0400

    Add `ctrlEngineSetups` user control for connection setup
    Introduced a new user control `ctrlEngineSetups` to manage
    connection string architecture selection. Updated the project
    file to include the new control's code, designer, and resource
    files.
    
    The control provides options for "App.Config" and "Static Class"
    architectures, dynamically updating the UI based on the user's
    selection. Designed the layout and implemented event handling
    for the combo box to enhance interactivity. Updated the
    `AssemblyReference.cache` file to reflect these additions.

---
+++ Code_Generator_Desktop_Application/Controls/ctrlEngineSetups.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Code_Generator_DApp.Controls
+{
+    partial class ctrlEngineSetups
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel5 = new Guna.UI2.WinForms.Guna2Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbConnectionType = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblConnectionDetails = new System.Windows.Forms.Label();
+            this.guna2Panel1.SuspendLayout();
+            this.guna2Panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.guna2Panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2Panel1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(65)))), ((int)(((byte)(80)))));
+            this.guna2Panel1.BorderRadius = 10;
+            this.guna2Panel1.BorderThickness = 1;
+            this.guna2Panel1.Controls.Add(this.lblConnectionDetails);
+            this.guna2Panel1.Controls.Add(this.guna2Panel2);
+            this.guna2Panel1.Controls.Add(this.cbConnectionType);
+            this.guna2Panel1.Controls.Add(this.label1);
+            this.guna2Panel1.Controls.Add(this.guna2Panel5);
+            this.guna2Panel1.Controls.Add(this.label4);
+            this.guna2Panel1.Location = new System.Drawing.Point(3, 3);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(512, 617);
+            this.guna2Panel1.TabIndex = 1;
+            // 
+            // guna2Panel5
+            // 
+            this.guna2Panel5.BorderRadius = 9;
+            this.guna2Panel5.Controls.Add(this.pictureBox3);
+            this.guna2Panel5.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(219)))), ((int)(((byte)(229)))));
+            this.guna2Panel5.Location = new System.Drawing.Point(18, 19);
+            this.guna2Panel5.Name = "guna2Panel5";
+            this.guna2Panel5.Size = new System.Drawing.Size(50, 50);
+            this.guna2Panel5.TabIndex = 24;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Code_Generator_DApp.Properties.Resources.settings;
+            this.pictureBox3.Location = new System.Drawing.Point(7, 7);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(36, 36);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 21F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(74, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 34);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Engine Setup";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(83, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 23);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Connection String Architecture";
+            // 
+            // cbConnectionType
+            // 
+            this.cbConnectionType.BackColor = System.Drawing.Color.Transparent;
+            this.cbConnectionType.BorderRadius = 9;
+            this.cbConnectionType.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbConnectionType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbConnectionType.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbConnectionType.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbConnectionType.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbConnectionType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cbConnectionType.ItemHeight = 30;
+            this.cbConnectionType.Items.AddRange(new object[] {
+            "App.Config",
+            "Static Class"});
+            this.cbConnectionType.Location = new System.Drawing.Point(25, 144);
+            this.cbConnectionType.Name = "cbConnectionType";
+            this.cbConnectionType.Size = new System.Drawing.Size(456, 36);
+            this.cbConnectionType.StartIndex = 0;
+            this.cbConnectionType.TabIndex = 26;
+            this.cbConnectionType.SelectedIndexChanged += new System.EventHandler(this.cbConnectionType_SelectedIndexChanged);
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BorderRadius = 9;
+            this.guna2Panel2.Controls.Add(this.pictureBox1);
+            this.guna2Panel2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(219)))), ((int)(((byte)(229)))));
+            this.guna2Panel2.Location = new System.Drawing.Point(25, 90);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(45, 45);
+            this.guna2Panel2.TabIndex = 27;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Code_Generator_DApp.Properties.Resources.url;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblConnectionDetails
+            // 
+            this.lblConnectionDetails.AutoSize = true;
+            this.lblConnectionDetails.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConnectionDetails.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(148)))), ((int)(((byte)(91)))));
+            this.lblConnectionDetails.Location = new System.Drawing.Point(29, 183);
+            this.lblConnectionDetails.Name = "lblConnectionDetails";
+            this.lblConnectionDetails.Size = new System.Drawing.Size(306, 19);
+            this.lblConnectionDetails.TabIndex = 31;
+            this.lblConnectionDetails.Text = "Store connection string in App.Config file.";
+            // 
+            // ctrlEngineSetups
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.guna2Panel1);
+            this.Name = "ctrlEngineSetups";
+            this.Size = new System.Drawing.Size(518, 623);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.guna2Panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.guna2Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2ComboBox cbConnectionType;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblConnectionDetails;
+    }
+}
---
+++ Code_Generator_Desktop_Application/Controls/ctrlEngineSetups.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Code_Generator_DApp.Controls
+{
+    public partial class ctrlEngineSetups : UserControl
+    {
+        public ctrlEngineSetups()
+        {
+            InitializeComponent();
+        }
+
+        private void cbConnectionType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string type = cbConnectionType.SelectedItem as string;
+            if(type == "App.Config")
+            {
+                lblConnectionDetails.Text = "Store connection string in App.Config file.";
+                lblConnectionDetails.ForeColor = Color.FromArgb(47, 148, 91);
+            }
+            else if (type == "Static Class")
+            {
+                lblConnectionDetails.Text = "Manage connection string in a static class.";
+                lblConnectionDetails.ForeColor = Color.FromArgb(100, 62, 196);
+            }
+        }
+    }
+}
