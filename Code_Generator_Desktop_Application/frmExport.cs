f3da142fc0b0f97d12429febb64102ba9df94107
abdullrahman <lodragon71@gmail.com>
2026-08-19 20:03:09 +0400

    Refactor frmExport constructor and add new fields
    Updated `using` directives to include `Code_Generator_Business_Layer.DataAccessGenerators`. Replaced the parameterless constructor in `frmExport` with a new constructor that accepts `DataAccessClass`, `BusinessClass`, `Queries`, and `clsConnectionData connections`.
    
    Introduced private fields `_DataAccessClass`, `_Business`, `_Queries`, and `_Connection` to store the constructor arguments. These changes improve the class's ability to handle external dependencies and data.

--- Code_Generator_Desktop_Application/frmExport.cs
+++ Code_Generator_Desktop_Application/frmExport.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Code_Generator_Business_Layer.DataAccessGenerators;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,18 @@ namespace Code_Generator_DApp
 {
     public partial class frmExport : Form
     {
-        public frmExport()
+        string _DataAccessClass = "";
+        string _Business = "";
+        string _Queries = "";
+        clsConnectionData _Connection;
+        public frmExport(string DataAccessClass, string BusinessClass, string Queries, clsConnectionData connections)
         {
             InitializeComponent();
+
+            _DataAccessClass = DataAccessClass;
+            _Business = BusinessClass;
+            _Queries = Queries;
+            _Connection = connections;
         }
     }
 }
