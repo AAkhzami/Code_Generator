a361d08a375378b3594322c424c80d499cf5f3e0
abdullrahman <lodragon71@gmail.com>
2026-08-19 18:22:20 +0400

    Refactor: Remove unused fields and properties
    Removed private fields and their corresponding public properties
    (`_DataAccessClass`, `_BusinessLayerClass`, `_Queries`, `_Connection`)
    from `ctrlPreviewAndGeneratePage`. These were used to manage values
    related to data access, business layer, queries, and connection types,
    but are no longer needed. This simplifies the class and reduces
    unnecessary complexity. The constructor remains unchanged.

--- Code_Generator_Desktop_Application/Controls/Preview_And_Generate_Page/ctrlPreviewAndGeneratePage.cs
+++ Code_Generator_Desktop_Application/Controls/Preview_And_Generate_Page/ctrlPreviewAndGeneratePage.cs
@@ -19,60 +19,6 @@ namespace Code_Generator_DApp.Controls.Preview_And_Generate_Page
 {
     public partial class ctrlPreviewAndGeneratePage : UserControl
     {
-
-        private string _DataAccessClass = "DataAccessClass";
-        private string _BusinessLayerClass = "BusinessLayerClass";
-        private string _Queries = "Queries";
-        private string _Connection = "ConnectionType";
-        public string DataAccessClass
-        {
-            get
-            {
-                return _DataAccessClass;
-            }
-            set
-            {
-                _DataAccessClass = value;
-                tcCodesGenerator.TabPages["tpDataAccess"].Text = value;
-            }
-        }
-        public string BusinessLayerClass
-        {
-            get
-            {
-                return _BusinessLayerClass;
-            }
-            set
-            {
-                _BusinessLayerClass = value;
-                tcCodesGenerator.TabPages["tpBusinessLayer"].Text = value;
-            }
-        }
-        public string Querys
-        {
-            get
-            {
-                return _Queries;
-            }
-            set
-            {
-                _Queries = value;                
-                tcCodesGenerator.TabPages["tpQueries"].Text = value;
-            }
-        }
-        public string Connection
-        {
-            get
-            {
-                return _Connection;
-            }
-            set
-            {
-                _Connection = value;                
-                tcCodesGenerator.TabPages["tpConnection"].Text = value;
-            }
-        }
-
         public ctrlPreviewAndGeneratePage()
         {
             InitializeComponent();
